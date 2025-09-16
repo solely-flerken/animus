287941c63ca2784f36074a1569ba7f9ac971f182
solely-flerken <solely.flerken@proton.me>
2025-09-16 19:44:47 +0200

    ✨ Global event system

---
+++ Assets/Events/EventSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Events
+{
+    public class EventSystem : MonoBehaviour
+    {
+        public static EventSystem Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public static event Action<string> OnChatMessage;
+
+        public static void InvokeChatMessage(string message)
+        {
+            OnChatMessage?.Invoke(message);
+        }
+    }
+}
