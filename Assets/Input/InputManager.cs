12135cabfc6882bb6cf1959d85403f241c38d13e
solely-flerken <solely.flerken@proton.me>
2025-09-16 19:46:00 +0200

    ✨ Instantiable Input Manager

---
+++ Assets/Input/InputManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Input
+{
+    public class InputManager : MonoBehaviour
+    {
+        public InputSystem_Actions InputActions;
+        public InputSystem_Actions.PlayerActions PlayerActions;
+        public InputSystem_Actions.UIActions UIActions;
+
+        private void Awake()
+        {
+            InputActions = new InputSystem_Actions();
+            PlayerActions = InputActions.Player;
+            UIActions = InputActions.UI;
+        }
+    }
+}
