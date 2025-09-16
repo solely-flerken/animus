267d0829468481379b4b217c47de9fcb8c70fa25
solely-flerken <solely.flerken@proton.me>
2025-09-16 19:45:01 +0200

    ✨ NPC event listener

---
+++ Assets/NPC/EventListener.cs
@@ -0,0 +1,23 @@
+﻿using Events;
+using UnityEngine;
+
+namespace NPC
+{
+    public class EventListener : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            EventSystem.OnChatMessage += HandleCommand;
+        }
+
+        private void OnDisable()
+        {
+            EventSystem.OnChatMessage -= HandleCommand;
+        }
+
+        private static void HandleCommand(string command)
+        {
+            Debug.Log(command);
+        }
+    }
+}
