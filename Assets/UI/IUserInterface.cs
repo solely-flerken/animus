bb35dd99a75ccbdee7fd5a079cd4662e6bd1921c
solely-flerken <solely.flerken@proton.me>
2025-09-16 19:50:49 +0200

    ✨ Introduce base classes for UI components

---
+++ Assets/UI/IUserInterface.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.UIElements;
+
+namespace UI
+{
+    public interface IUserInterface
+    {
+        VisualElement Root { get; set; }
+        bool IsVisible => Root.style.display == DisplayStyle.Flex;
+    }
+}
---
+++ Assets/UI/UserInterfaceBase.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    public abstract class UserInterfaceBase : MonoBehaviour, IUserInterface
+    {
+        public VisualElement Root { get; set; }
+        public bool IsVisibleInitially { get; set; }
+        public bool IsVisible => Root?.style.display == DisplayStyle.Flex;
+
+        protected virtual void Show()
+        {
+            Root.style.display = DisplayStyle.Flex;
+        }
+
+        protected virtual void Hide()
+        {
+            Root.style.display = DisplayStyle.None;
+        }
+    }
+}
