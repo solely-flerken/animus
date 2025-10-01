e71524ba550a230cac25723679e13ac7912f977f
solely-flerken <solely.flerken@proton.me>
2025-10-02 00:32:46 +0200

    ✨ add Points of Interest

---
+++ Assets/Packages/Animus.Unity/Runtime/Environment/PointOfInterest/PointOfInterest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Packages.Animus.Unity.Runtime.Environment.PointOfInterest
+{
+    public class PointOfInterest : MonoBehaviour
+    {
+        private void Start()
+        {
+            PointOfInterestManager.Instance?.RegisterPoi(this);
+        }
+
+        private void OnDisable()
+        {
+            PointOfInterestManager.Instance?.UnregisterPoi(this);
+        }
+    }
+}
