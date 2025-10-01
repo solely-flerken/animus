bd52ed073197d4c6ad75e448eddd5bde5623cae1
solely-flerken <solely.flerken@proton.me>
2025-10-02 00:34:09 +0200

    🚧 simple navigation

--- Assets/Packages/Animus.Unity/Runtime/Agent/Agent.cs
+++ Assets/Packages/Animus.Unity/Runtime/Agent/Agent.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using Packages.Animus.Unity.Runtime.Core;
 using Packages.Animus.Unity.Runtime.Data;
+using Packages.Animus.Unity.Runtime.Environment.PointOfInterest;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Packages.Animus.Unity.Runtime.Agent
 {
+    [RequireComponent(typeof(NavMeshAgent))]
     public class Agent : MonoBehaviour
     {
         public AgentModel agentModel;
+
+        private NavMeshAgent _navMeshAgent;
+        private Vector3 _currentTargetPosition;
 
         private async void Start()
         {
             try
             {
+                _navMeshAgent = GetComponent<NavMeshAgent>();
+
                 var service = AnimusServiceManager.Service;
                 agentModel = await service.RegisterAgent(agentModel);
             }
@@ -20,6 +28,12 @@ namespace Packages.Animus.Unity.Runtime.Agent
             {
                 Debug.LogError(e);
             }
+        }
+
+        public void GoToPoi(PointOfInterest poi)
+        {
+            _currentTargetPosition = poi.transform.position;
+            _navMeshAgent.SetDestination(_currentTargetPosition);
         }
     }
 }
