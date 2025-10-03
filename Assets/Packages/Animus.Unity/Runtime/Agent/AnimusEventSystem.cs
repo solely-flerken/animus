2b31d9b4f82d2b4aaa4b4bd43ea81c63adb35047
solely-flerken <solely.flerken@proton.me>
2025-10-04 01:09:35 +0200

    ✨ global Animus event system

---
+++ Assets/Packages/Animus.Unity/Runtime/Agent/AnimusEventSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Packages.Animus.Unity.Runtime.Agent
+{
+    public class AnimusEventSystem : MonoBehaviour
+    {
+        public static AnimusEventSystem Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public static event Action<string> OnLlmResponse;
+
+        public static void InvokeLlmResponse(string message)
+        {
+            OnLlmResponse?.Invoke(message);
+        }
+    }
+}
