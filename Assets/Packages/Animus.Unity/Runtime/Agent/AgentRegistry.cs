68f6eaddb4430e5d0d00ed5f03252a879c5a587a
solely-flerken <solely.flerken@proton.me>
2025-10-03 23:51:02 +0200

    ✨ global agent registry/catalog

---
+++ Assets/Packages/Animus.Unity/Runtime/Agent/AgentRegistry.cs
@@ -0,0 +1,12 @@
+﻿using Packages.Animus.Unity.Runtime.Core;
+
+namespace Packages.Animus.Unity.Runtime.Agent
+{
+    public class AgentRegistry : TypeRegistry<AgentRegistry, Agent>
+    {
+        public Agent FindByGameKey(string gameKey)
+        {
+            return allItems.Find(a => a.agentModel.gameKey == gameKey);
+        }
+    }
+}
