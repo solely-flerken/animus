c0f4284d51d67d52accd8146fcc3b28497875a0d
solely-flerken <solely.flerken@proton.me>
2025-10-23 00:16:19 +0200

    ♻️ add description field to actions

--- Assets/Packages/Animus.Unity/Runtime/Agent/Actions/NpcAction.cs
+++ Assets/Packages/Animus.Unity/Runtime/Agent/Actions/NpcAction.cs
@@ -6,6 +6,7 @@ namespace Packages.Animus.Unity.Runtime.Agent.Actions
     public abstract class NpcAction : ScriptableObject
     {
         public string actionKey;
+        public string description;
         public abstract void Execute(AnimusAgent animusAgent, Dictionary<string, object> payload);
     }
 }
