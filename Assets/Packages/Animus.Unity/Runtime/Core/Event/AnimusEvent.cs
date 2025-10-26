e84f9a58297b52d364f5a561d5ca5be11934ebbe
solely-flerken <solely.flerken@proton.me>
2025-10-26 18:11:28 +0100

    ✨ add custom JsonConverter for AnimusEntity to serialize only gameKeys

--- Assets/Packages/Animus.Unity/Runtime/Core/Event/AnimusEvent.cs
+++ Assets/Packages/Animus.Unity/Runtime/Core/Event/AnimusEvent.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using Packages.Animus.Unity.Runtime.Core.Entity;
+using Packages.Animus.Unity.Runtime.Core.Utils.Json;
+using Unity.Plastic.Newtonsoft.Json;
 using UnityEngine;
 
 namespace Packages.Animus.Unity.Runtime.Core.Event
@@ -7,8 +9,13 @@ namespace Packages.Animus.Unity.Runtime.Core.Event
     public class AnimusEvent
     {
         public AnimusEventType EventType { get; set; }
+
+        [JsonConverter(typeof(AnimusEntityGameKeyConverter))]
         public AnimusEntity EventSource { get; set; }
+
+        [JsonConverter(typeof(AnimusEntityListGameKeyConverter))]
         public List<AnimusEntity> EventTarget { get; set; }
+
         public Vector3 EventLocation { get; set; }
     }
 }
---
+++ Assets/Packages/Animus.Unity/Runtime/Core/Utils/Json/AnimusEntityJsonConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Packages.Animus.Unity.Runtime.Core.Entity;
+using Unity.Plastic.Newtonsoft.Json;
+
+namespace Packages.Animus.Unity.Runtime.Core.Utils.Json
+{
+    public class AnimusEntityGameKeyConverter : JsonConverter<AnimusEntity>
+    {
+        public override void WriteJson(JsonWriter writer, AnimusEntity value, JsonSerializer serializer)
+        {
+            if (value == null)
+            {
+                writer.WriteNull();
+            }
+            else
+            {
+                writer.WriteValue(value.gameKey);
+            }
+        }
+
+        public override AnimusEntity ReadJson(JsonReader reader, Type objectType, AnimusEntity existingValue,
+            bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AnimusEntityListGameKeyConverter : JsonConverter<List<AnimusEntity>>
+    {
+        public override void WriteJson(JsonWriter writer, List<AnimusEntity> value, JsonSerializer serializer)
+        {
+            var keys = value?.Select(e => e?.gameKey).ToList() ?? new List<string>();
+            serializer.Serialize(writer, keys);
+        }
+
+        public override List<AnimusEntity> ReadJson(JsonReader reader, Type objectType,
+            List<AnimusEntity> existingValue, bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
