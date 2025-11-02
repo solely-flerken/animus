d7c9f78c36a72508d295f9e9aca466dbfc63adcc
solely-flerken <solely.flerken@proton.me>
2025-11-03 00:56:04 +0100

    ♻️ refactor

--- Assets/Packages/Animus.Unity/Runtime/Core/AI/PromptContext.cs
+++ Assets/Packages/Animus.Unity/Runtime/Core/AI/PromptContext.cs
@@ -9,28 +9,28 @@ namespace Packages.Animus.Unity.Runtime.Core.AI
 {
     public class PromptContext
     {
-        [JsonProperty("persona", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("Persona", NullValueHandling = NullValueHandling.Ignore)]
         public string Persona { get; set; }
 
-        [JsonProperty("available_actions", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("AvailableActions", NullValueHandling = NullValueHandling.Ignore)]
         public List<NpcAction> AvailableActions { get; set; }
 
-        [JsonProperty("relevant_memories", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("RelevantMemories", NullValueHandling = NullValueHandling.Ignore)]
         public List<string> RelevantMemories { get; set; }
 
-        [JsonProperty("recent_events", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("RecentEvents", NullValueHandling = NullValueHandling.Ignore)]
         public List<AnimusEvent> RecentEvents { get; set; }
 
-        [JsonProperty("environment_description", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("EnvironmentDescription", NullValueHandling = NullValueHandling.Ignore)]
         public EnvironmentSnapshot Environment { get; set; }
 
-        [JsonProperty("conversation_history", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("ConversationHistory", NullValueHandling = NullValueHandling.Ignore)]
         public List<DialogLine> ConversationHistory { get; set; }
 
-        [JsonProperty("rules", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("Rules", NullValueHandling = NullValueHandling.Ignore)]
         public List<string> Rules { get; set; }
 
-        [JsonProperty("task_instruction")] 
+        [JsonProperty("TaskInstruction")] 
         public string TaskInstruction { get; set; } = TaskInstructions.NextAction;
     }
 }
