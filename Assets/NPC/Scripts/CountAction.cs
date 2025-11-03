8f24e719dbffd8bcbd3831af6aea2da52fdccdb9
solely-flerken <solely.flerken@proton.me>
2025-11-03 01:16:40 +0100

    🍱 add CountAction for async testing

---
+++ Assets/NPC/Scripts/CountAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Events;
+using Packages.Animus.Unity.Runtime.Agent;
+using Packages.Animus.Unity.Runtime.Agent.Actions;
+using UnityEngine;
+
+namespace NPC.Scripts
+{
+    [CreateAssetMenu(fileName = "CountAction", menuName = "Animus/NPC/Action/Count")]
+    public class CountAction : NpcAction
+    {
+        public override async void Execute(AnimusAgent animusAgent, Dictionary<string, object> payload)
+        {
+            for (var i = 1; i <= 10; i++)
+            {
+                EventSystem.InvokeDisplayMessageInChat($"{animusAgent.gameKey}: {i.ToString()}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
