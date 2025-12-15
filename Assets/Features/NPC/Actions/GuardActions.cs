48fac90c22ba492b91e50006f7ea2cfba987c9c6
solely-flerken <solely.flerken@proton.me>
2025-12-15 22:45:22 +0100

    ✨ guard actions

---
+++ Assets/Features/NPC/Actions/GuardActions.cs
@@ -0,0 +1,26 @@
+﻿using Cysharp.Threading.Tasks;
+using Features.Gate.Scripts;
+using Packages.Animus.Unity.Runtime.Core.Actions;
+using UnityEngine;
+
+namespace Features.NPC.Actions
+{
+    public class GuardActions : MonoBehaviour
+    {
+        [SerializeField] private InteractableGate interactableGate;
+        
+        [AgentAction("open_gate", "Open the Gate.")]
+        public UniTask<string> OpenGate()
+        {
+            interactableGate.OpenGate();
+            return UniTask.FromResult("success: opening gate");
+        }
+
+        [AgentAction("close_gate", "Close the Gate.")]
+        public UniTask<string> CloseGate()
+        {
+            interactableGate.CloseGate();
+            return UniTask.FromResult("success: closing gate");
+        }
+    }
+}
