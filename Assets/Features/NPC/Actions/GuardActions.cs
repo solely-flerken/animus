0602c137f1d86f958f8b8aa71858362f4f748100
solely-flerken <solely.flerken@proton.me>
2026-01-23 01:50:13 +0100

    ✨ guard work action

--- Assets/Features/NPC/Actions/GuardActions.cs
+++ Assets/Features/NPC/Actions/GuardActions.cs
@@ -1,28 +1,56 @@
 ﻿using Cysharp.Threading.Tasks;
 using Features.Gate.Scripts;
+using Features.Goap.Agents;
 using Packages.Animus.Unity.Runtime.Core.Actions;
+using Packages.Animus.Unity.Runtime.Core.Entity;
+using Packages.Animus.Unity.Runtime.Modules.Conversation;
 using UnityEngine;
 
 namespace Features.NPC.Actions
 {
-    [RequireComponent(typeof(AgentActionSystem))]
+    [RequireComponent(typeof(AnimusAgent), typeof(AgentActionSystem), typeof(SimpleAgentBrain))]
     public class GuardActions : MonoBehaviour
     {
         private AgentActionSystem _actionSystem;
+        private AnimusAgent _agent;
+        private SimpleAgentBrain _brain;
 
+        [SerializeField] private AnimusLocation guardPost;
         [SerializeField] private InteractableGate interactableGate;
 
         private void Awake()
         {
             _actionSystem = GetComponent<AgentActionSystem>();
+            _agent = GetComponent<AnimusAgent>();
+            _brain = GetComponent<SimpleAgentBrain>();
         }
 
         private void Start()
         {
+            RegisterWork();
             RegisterOpenGate();
             RegisterCloseGate();
         }
 
+        private void RegisterWork()
+        {
+            var workAction = new AgentAction("work", "Stand guard at your post",
+                logic: async _ =>
+                {
+                    if (guardPost == null)
+                    {
+                        Debug.LogError($"{nameof(guardPost)} is null");
+                        return string.Empty;
+                    }
+
+                    return await StandGuard(guardPost);
+                },
+                condition: null
+            );
+
+            _actionSystem.RegisterAction(workAction);
+        }
+        
         private void RegisterOpenGate()
         {
             var openGateAction = new AgentAction("open_gate", "Open the gate.",
@@ -61,6 +89,25 @@ namespace Features.NPC.Actions
             );
 
             _actionSystem.RegisterAction(openGateAction);
+        }
+        
+        private async UniTask<string> StandGuard(AnimusLocation location)
+        {
+            if (ConversationAnchor.ConversationAnchors.TryGetValue(_agent.gameKey, out var anchor))
+            {
+                anchor.RemoveParticipant(_agent.gameKey);
+            }
+
+            _agent.memories.Add("On the way to the guard post...");
+            
+            await _brain.StartGoalMoveTo(location.transform);
+
+            _agent.memories.Add("Currently standing guard.");
+            
+            // Runs indefinitely. Working should be stopped only when performing another action.
+            await UniTask.WaitUntilCanceled(this.GetCancellationTokenOnDestroy());
+            
+            return "Finished standing guard.";
         }
     }
 }
